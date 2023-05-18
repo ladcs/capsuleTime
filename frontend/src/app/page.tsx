b1c23408ad8e6446f7c11ed4c381c48c8f3f5342
ladcs <hpladcs@gmail.com>
2023-05-18 18:35:34 -0300

    first steps

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="font-bold text-4xl">Alguma coisa aqui</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div
+          className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full border-r border-white/10 bg-purple-700 opacity-50 blur-full
+        "
+        />
+        {/* cria o semi-circulo no lado direito desse quadrante. blur */}
+
+        {/* stripes */}
+        <div
+          className="absolute bottom-0 right-2 top-0 w-2  bg-stripes
+        "
+        />
+      </div>
+      {/* right */}
+      <div className="flex flex-col p-16">
+        {/* bg-[url(path)] coloca bg image e bg-cover pega o background inteiro */}
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
